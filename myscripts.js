31e80fd66f02e5349e2aa59d01694cd279f315a0
SonaliMathur <sonaliuae@gmail.com>
2022-12-05 13:34:17 +0000

    :)

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,5 @@
 //Responsive navigation bar - becomes little icons when the screen width is reduced.
-//Use of Conditional Statements (1)
+//Use of Conditional Statements (1) in a Function (1)
 function myNavFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "allTopNav") {
@@ -9,4 +9,16 @@ function myNavFunction() {
     }
   }
 
-  //rotation of round images with scroll pattern
+  //Quick Add Button - colour changes for a bit when pressed
+  //Use of function (2)
+
+  const btn = document.getElementById('btn');
+  index0 = 0
+  index1 = 1
+
+  const colours = ['green', '#D37267'];
+
+  btn.addEventListener('Click', function buttonColour() {
+      buttonColour.style.backgroundColor = colours[Index];
+      index = index + 1
+  });
