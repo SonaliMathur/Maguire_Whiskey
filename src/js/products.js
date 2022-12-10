39ecd8406b8441038e4b87724c4eb546ec179411
Luke McAuley <55586654+LukeMcAuleyDublin@users.noreply.github.com>
2022-12-10 14:55:32 +0000

    peepeepoopoo

--- src/js/products.js
+++ src/js/products.js
@@ -2,57 +2,57 @@
 //https://stackoverflow.com/questions/65237381/trying-to-filter-an-array-of-products-with-an-array-of-filters-upon-click
 const products = [
     {
-        id: 1,
-        name: 'Negroni Kit',
+        id: 0,
+        productId: 0,
+        name: 'Number 1 Kit',
         price: 25.99,
         img_src: '../../Products/ItemA.png',
-        descriptionOne: "gin, vermouth, shit in my ass",
+       
         reference: "./negroniKit.html"
-        // descriptionTwo: "Fortnite battle pass"
     },
     {
-        id: 2,
-        name: 'The 1829 Kit',
+        id: 0,
+        productId: 1,
+        name: 'Number 2 Kit',
         price: 25.99,
         img_src: '../../Products/ItemB.png',
-        descriptionOne: "gin, vermouth, shit in my ass",
+     
         reference: "../negroniKit.html",
-        // descriptionTwo: "Fortnite battle pass"
     },
     {
-        id: 3,
-        name: 'Negroni Kit',
+        id: 0,
+        productId: 2,
+        name: 'Number 3 Kit',
         price: 25.99,
         img_src: '../../Products/ItemC.png',
-        descriptionOne: "gin, vermouth, shit in my ass",
+       
         reference: "../negroniKit.html",
-        // descriptionTwo: "Fortnite battle pass"
     },
     {
-        id: 4,
-        name: 'Negroni Kit',
+        id: 0,
+        productId: 3,
+        name: 'Number 4 Kit',
         price: 25.99,
         img_src: '../../Products/ItemD.png',
-        descriptionOne: "gin, vermouth, shit in my ass",
+       
         reference: "../negroniKit.html",
-        // descriptionTwo: "Fortnite battle pass"
     },
     {
-        id: 5,
-        name: 'Negroni Kit',
+        id: 0,
+        productId: 4,
+        name: 'Number 5 Kit',
         price: 25.99,
         img_src: '../../Products/ItemE.png',
-        descriptionOne: "gin, vermouth, shit in my ass",
-        // descriptionTwo: "Fortnite battle pass"
+       
     },
     {
-        id: 6,
-        name: 'Negroni Kit',
+        id: 0,
+        productId: 5,
+        name: 'Number 6 Kit',
         price: 25.99,
         img_src: '../../Products/ItemF.png',
-        descriptionOne: "gin, vermouth, shit in my ass", 
+      
         reference: "../negroniKit.html",
-        // descriptionTwo: "Fortnite battle pass"
     }
 ]
 
@@ -60,162 +60,176 @@ const products = [
 // https://stackoverflow.com/questions/71415643/foreach-method-in-javascript-for-key-value-pairs
 function displayProducts() {
     var product_div = document.getElementById('products'); // The div in the HTML I want to input into
-    var productCount = 1; // I want 3 products per row, counting so JS knows when to input new <div class ="row">
+    var productCount = 0; // I want 3 products per row, counting so JS knows when to input new <div class ="row">
     let row = `<div class=row>`
-    products.forEach(x => {
-        if (productCount <= 2) {
+    products.forEach(item => {
+        if (productCount < 2) {
             row += `
             <div class="column">
-                <a href="${x.reference}"><img src="${x.img_src}" alt="cocktail image" style="width:100%""></a>
+                <a href="${item.reference}"><img src="${item.img_src}" alt="cocktail image" style="width:100%""></a>
                 <div class=" divider2">
                 </div>
-                <h2>${x.name}</h2>
-                <p class="price">€${x.price}</p>
+                <h2>${item.name}</h2>
+                <p class="price">€${item.price}</p>
                 <div class="information2">
-                    <p class="info">${x.descriptionOne}</p>
+                    
                    
             
                 </div>
                 <div class="quickButton">
-                    <input id="button_${x.id}" class="quickAddButton" type="button" value="+ QUICK ADD" onclick="tick(${x.id})">
+                    <input id="button_${item.id}" class="quickAddButton" type="button" value="+ QUICK ADD" onclick="addItem(products[${item.productId}])">
                 </div>
             </div>
-
-            
             `
             productCount++;
 
         } else {
             row += `
             <div class="column">
-            <a href="${x.reference}"><img src="${x.img_src}" alt="cocktail image" style="width:100%""></a>
+            <a href="${item.reference}"><img src="${item.img_src}" alt="cocktail image" style="width:100%""></a>
                 <div class=" divider2">
                 </div>
-                <h2>${x.name}</h2>
-                <p class="price">€${x.price}</p>
+                <h2>${item.name}</h2>
+                <p class="price">€${item.price}</p>
                 <div class="information2">
-                    <p class="info">${x.descriptionOne}</p>
+                  
                    
                 </div>
                 <div class="quickButton">
-                    <input id="button_${x.id}" class="quickAddButton" type="button" value="+ QUICK ADD" onclick="tick(${x.id})">
+                    <input id="button_${item.id}" class="quickAddButton" type="button" value="+ QUICK ADD" onclick="addItem(products[${item.productId}])">
                 </div>
             </div>
             </div>
             `
-            productCount = 1;
-            console.log(productCount);
+            productCount = 0;
             product_div.innerHTML += row;
             row = `<div class="row">`
         }
     })
-    
-    
 }
 
 
 
+function displayCartItems() {
+    let cartItems = document.getElementById('cart-items');
+    let cartItemCount = 0;
+    let row = `<div class="row">`;
+    let rows = "";
+    if (shoppingCart.length == 0) {
+        row += `
+            <div class="column">
+                <h2>Cart is empty</h2>
+            </div>
+            </div>
+        `
+        cartItems.innerHTML = row;
+    } else {
+        let emptyButton = document.getElementById('empty-cart-button');
+        emptyButton.innerHTML = `
+        <div class="quickButton">
+            <input class="quickAddButton" type="button" value="Empty Cart" onclick="clearCart()">
+        </div>
+        `
+    }
+    shoppingCart.forEach(item => {
+        if (cartItemCount < 2) {
+            row = `
+            <div class="column">
+                <a href="${item.reference}"><img src="${item.img_src}" alt="cocktail image" style="width:100%""></a>
+                <div class=" divider2">
+                </div>
+                <h2>${item.name}</h2>
+                <p class="price">€${item.price}</p>
+                <div class="information2">
+                    
+                </div>
+                <div class="quickButton">
+                    <input id="button_${item.id}" class="quickAddButton" type="button" value="- REMOVE" onclick="removeItem(${item.id})">
+                </div>
+            </div>
+            `
+            rows += row;
+            cartItemCount++;
+
+        } else {
+            row = `
+            <div class="column">
+            <a href="${item.reference}"><img src="${item.img_src}" alt="cocktail image" style="width:100%""></a>
+                <div class=" divider2">
+                </div>
+                <h2>${item.name}</h2>
+                <p class="price">€${item.price}</p>
+                <div class="information2">
+                    
+                </div>
+                <div class="quickButton">
+                    <input id="button_${item.id}" class="quickAddButton" type="button" value="- REMOVE" onclick="removeItem(${item.id})">
+                </div>
+            </div>
+            </div>
+            `
+            cartItemCount = 0;
+            rows += row;
+        }
+        cartItems.innerHTML = rows;
+    })
+}
 
 
-function addToCart(productId) {
-    // Find the product by its id in the product array
-    const product = products.find(product => product.id === productId);
 
-    // If the product was found, add it to the shopping cart stored in local storage
-    if (product) {
-        // Get the current shopping cart from local storage
-        let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart')) || [];
+let shoppingCart = [];
 
-        // Add the product to the shopping cart
-        shoppingCart.push(product);
+// https://www.w3docs.com/snippets/javascript/how-to-clone-a-javascript-object.html
+function copy(mainObject) {
+    let objectCopy = {}; // objectCopy will store a copy of the mainObject
+    let key;
+    for (key in mainObject) {
+        objectCopy[key] = mainObject[key]; // copies each property to the objectCopy object
+    }
+    return objectCopy;
+}
 
-        // Save the updated shopping cart to local storage
+function addItem(item) {
+    let new_item = copy(item);
+    new_item.id = shoppingCart.length + 1;
+    shoppingCart.push(new_item);
+    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+}
+
+function removeItem(id) {
+    let index = getCartIndex(id);
+    if (index > -1) {
+        shoppingCart.splice(index, 1);
         localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
     }
 }
 
-// Function to get the total price of all products in the shopping cart
+function clearCart() {
+    shoppingCart = [];
+    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+}
+
 function getTotal() {
-    // Get the shopping cart from local storage
-    const shoppingCart = JSON.parse(localStorage.getItem('shoppingCart')) || [];
-
-    // Reduce the shopping cart array to the sum of all prices
-    const total = shoppingCart.reduce((acc, products) => acc + products.price, 0);
-
-    // Return the total price
+    let total = 0;
+    for (let i = 0; i < shoppingCart.length; i++) {
+        total += shoppingCart[i].price;
+    }
     return total;
 }
 
-// Example usage:
-
-// Add a product to the shopping cart
-addToCart(2);
-
-// Get the total price of all products in the shopping cart
-const total = getTotal();
-
-
-const addToCartButton = document.getElementsByClassName('quickButton');
-
-// Add an event listener to the button that calls the addToCart function when the button is clicked
-addToCartButton.onclick = () => {
-    // Call the addToCart function and pass it the product id
-    addToCart(2);
-};
-
-
-function goToCart() {
-    // Redirect the user to the cart page
-    window.location.href = './shoppingCart.html';
+// Load the shopping cart from local storage when the page loads
+let storedCart = localStorage.getItem('shoppingCart');
+if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
 }
 
-// // Function to display the contents of the shopping cart on the cart page
-// function displayCart() {
-//     // Get the shopping cart from local storage
-//     const shoppingCart = JSON.parse(localStorage.getItem('shoppingCart')) || [];
-// }
-
-    // // Get the element where the cart items will be displayed
-    // const cartListElement = document.getElementById('cart-list');
-
-    // // Loop through the shopping cart array and create a list item for each product
-    // for (const product of shoppingCart) {
-    //     // Create a new list item element
-    //     const listItem = document.createElement('li');
-
-    //     // Set the text content of the list item to the product name and price
-    //     listItem.textContent = `${product.name}: $${product.price}`;
-    // }
-
-//         // Add the list item to the cart list
-//         cartListElement.appendChild(listItem);
-//     }
-
-//     // Get the total price element
-//     const totalPriceElement = document.getElementById('total-price');
-
-//     // Set the text content of the total price element to the total price of the products in the cart
-//     totalPriceElement.textContent = `Total: $${getTotal()}`;
-// }
-
-// Example usage:
-
-// Add a product to the shopping cart
-// addToCart(2);
-
-// // When the user clicks the "Go to cart" button, redirect them to the cart page
-// const goToCartButton = document.getElementById('go-to-cart-button');
-// goToCartButton.addEventListener('click', goToCart);
-
-// // When the cart page loads, display the contents of the shopping cart
-// window.addEventListener('load', displayCart);
 
 
-
-
-
-
-
+function getCartIndex(id) {
+    return shoppingCart.findIndex(function (item) {
+        return item.id === id;
+    });
+}
 
 
 var negroniDrinkArray = [
